baa0968534e61cb89eed0e0ccf955d85375bb46b
Asad Faraz <asadfaraz44@gmail.com>
2021-04-06 22:20:30 +0500

    read more button added & css added

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 This app will fetch tours destination list from an api and I can do different operations on the list
 using React js.
+The tour destinations are displayed as cards and every card have 2 buttons and a price tags.
+
+User can click on the read more button to read all the info about a destination. If a user is not interested in a
+tour destination he can simply remove it by clicking the not intersted button on the card below.
--- src/components/Tour.js
+++ src/components/Tour.js
@@ -2,7 +2,11 @@ import React from "react";
 import classes from "./tour.module.css";
 
 const Tour = (props) => {
+  //   destructuring props
   const { image, info, name, price } = props.tour;
+
+  // usestate for read more button
+  const [readMore, setReadMore] = React.useState(true);
 
   return (
     <main className={classes.main}>
@@ -13,9 +17,22 @@ const Tour = (props) => {
             <h3>{name}</h3>
             <h4>${price}</h4>
           </div>
-          <p>{info}</p>
+          <p>
+            {readMore ? `${info.substring(0, 200)}...` : info}
+            <button
+              type="button"
+              className={classes.rmorebtn}
+              onClick={() => {
+                setReadMore(!readMore);
+              }}
+            >
+              {readMore ? "Read More" : "Show Less"}
+            </button>
+          </p>
         </section>
-        <button type="button">Not Interested</button>
+        <button type="button" className={classes.btn}>
+          Not Interested
+        </button>
       </article>
     </main>
   );
